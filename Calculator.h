91eafe1a5b15310588fbc32e93c8bb781f324419
SmetaninaKatherina <smetanina.e653@gmail.com>
2022-12-24 10:49:29 +0300

    Initial commit

---
+++ Calculator.h
@@ -0,0 +1,71 @@
+
+
+///Sign
+///
+/// @param SUM - +.
+/// @param MULT - *.
+/// @param SUB - -.
+/// @param DIV - /.
+enum Sign {
+    SUM,
+    MULT,
+    SUB,
+    DIV
+};
+
+/// parseSign - determine sign
+///
+/// \param sign - operation in char
+/// \return Sign (+ or - or * or /)
+Sign parseSign(char sign) {
+    if (sign == '+') {
+        return SUM;
+    } else if (sign == '-') {
+        return SUB;
+    } else if (sign == '*') {
+        return MULT;
+    } else if (sign == '/') {
+        return DIV;
+    } else {
+        std::cout << "Incorrect format";
+        return SUM;
+    }
+}
+
+/// sum
+///
+/// \param a
+/// \param b
+/// \return a + b
+int sum(int a, int b) {
+    return a + b;
+}
+
+/// sub
+///
+/// \param a
+/// \param b
+/// \return a - b
+int sub(int a, int b) {
+    return a - b;
+}
+
+/// mult
+///
+/// \param a
+/// \param b
+/// \return a * b
+int mult(int a, int b) {
+    return a * b;
+}
+
+/// divis
+///
+/// \param a
+/// \param b
+/// \return a / b
+int divis(int a, int b) {
+    return a / b;
+}
+
+
---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <gtest/gtest.h>
+#include "Calculator.h"
+
+TEST(BasicTests, SumCheck) {
+    ASSERT_EQ(parseSign('+'), SUM);
+    EXPECT_EQ(sum(1, 2), 3);
+    EXPECT_EQ(sum(657, 111), 768);
+}
+
+TEST(BasicTests, SubCheck) {
+    ASSERT_EQ(parseSign('-'), SUB);
+    EXPECT_EQ(sub(1, 2), -1);
+    EXPECT_EQ(sub(657, 111), 546);
+}
+
+TEST(BasicTests, MultCheck) {
+    ASSERT_EQ(parseSign('*'), MULT);
+    EXPECT_EQ(mult(1, 2), 2);
+    EXPECT_EQ(mult(657, 111), 72927);
+}
+
+TEST(BasicTests, DivCheck) {
+    ASSERT_EQ(parseSign('/'), DIV);
+    EXPECT_EQ(divis(1, 2), 0);
+    EXPECT_EQ(divis(657, 111), 5);
+}
